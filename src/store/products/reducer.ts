dba071e3fb681cdf425ff79ac0a3e97c5aee0418
isvk <info@isvk.ru>
2020-11-21 20:37:53 +0300

    fix. redux store for product :bug:

--- src/store/products/reducer.ts
+++ src/store/products/reducer.ts
@@ -1,19 +1,19 @@
-import { List } from "immutable";
+import { Map } from "immutable";
 import { ActionTypesInfer } from "src/store/actionTypes";
 import Product from "src/models/product";
 import * as types from "./types";
 import * as actions from "./actions";
 
-export type TStoreProduct = List<Product>;
+export type TStoreProduct = Map<Product["id"], Product>;
 
-const reducer = (state: TStoreProduct = List([]), action: ActionTypesInfer<typeof actions>) => {
+const reducer = (state: TStoreProduct = Map(), action: ActionTypesInfer<typeof actions>) => {
     switch (action.type) {
         case types.LOAD_PRODUCTS:
             return state;
 
         case types.ADD_PRODUCTS:
             action.products.forEach((product: Product) => {
-                state = state.push(product);
+                state = state.set(product.id, product);
             });
             return state;
 
