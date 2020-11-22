0460dc24ea953a846241490eda2d0a69b9693f68
isvk <info@isvk.ru>
2020-11-22 21:02:37 +0300

    fix. TS :bug:

--- src/store/actionTypes.ts
+++ src/store/actionTypes.ts
@@ -1,7 +1,8 @@
+import * as mainActions from "src/store/main/actions";
 import * as parameterActions from "src/store/parameters/actions";
 import * as productActions from "src/store/products/actions";
 
 type InferValueTypes<T> = T extends { [key: string]: infer U } ? U : never;
 export type ActionTypesInfer<T extends { [key: string]: any }> = ReturnType<InferValueTypes<T>>;
 
-export type allActionsTypes = ActionTypesInfer<typeof parameterActions | typeof productActions>;
+export type allActionsTypes = ActionTypesInfer<typeof mainActions | typeof parameterActions | typeof productActions>;
